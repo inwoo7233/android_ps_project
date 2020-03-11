c4fe31f7838b5eeead810c5f283f46962adc04a0
inwoo7233 <inwoo7233@gmail.com>
2020-03-11 13:31:09 +0900

    Pervert_app complete

---
+++ Perverteradication/app/src/main/java/com/example/perverteradication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.perverteradication
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        dontTouchButton.setOnClickListener() {
+            val soundFile = MediaPlayer.create(this, R.raw.myeye)
+            soundFile.start()
+        }
+
+        callButton.setOnClickListener() {
+            val intentVal = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tell:112")
+            startActivity(intentVal)
+        }
+    }
+}
